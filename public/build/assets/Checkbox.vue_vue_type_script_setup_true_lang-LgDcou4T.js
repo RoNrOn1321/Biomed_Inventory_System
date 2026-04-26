3360d88b4117a6db5202700f6b74a33f389ad02e
castanares <juliancastanares1@gmail.com>
2026-04-26 12:19:07 +0800

    Switch session driver, update host, rebuild assets
    Change SESSION_DRIVER from database to file and update REVERB_HOST from localhost to astmmc.biomed.com in .env. Regenerate frontend build artifacts (many files in public/build/assets were added/removed/renamed and manifest.json updated). Also include new session files under storage/framework/sessions. These changes align environment config with the deployed host and include the updated compiled frontend output.

---
+++ public/build/assets/Checkbox.vue_vue_type_script_setup_true_lang-LgDcou4T.js
@@ -0,0 +1,6 @@
+import{c as u,S as p,j as f,a as m,W as h}from"./createLucideIcon-D4HeXj6f.js";import{d as k,p as g,o as b,i as y,w as o,a as r,u as e,D as _,G as v}from"./app-Od3fzdO9.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=u("CheckIcon",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]),w=k({__name:"Checkbox",props:{defaultChecked:{type:Boolean},checked:{type:[Boolean,String]},disabled:{type:Boolean},required:{type:Boolean},name:{},value:{},id:{},asChild:{type:Boolean},as:{},class:{}},emits:["update:checked"],setup(c,{emit:d}){const s=c,n=d,i=g(()=>{const{class:a,...t}=s;return t}),l=p(i,n);return(a,t)=>(b(),y(e(h),v(e(l),{class:e(m)("peer size-5 shrink-0 rounded-sm border border-input ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=checked]:border-accent-foreground",s.class)}),{default:o(()=>[r(e(f),{class:"flex h-full w-full items-center justify-center text-current"},{default:o(()=>[_(a.$slots,"default",{},()=>[r(e(B),{class:"size-3.5 stroke-[3]"})])]),_:3})]),_:3},16,["class"]))}});export{w as _};
